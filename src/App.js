66b37a404fc7e3f3bc563c825c51242717a31cf9
root <root@localhost>
2020-11-17 13:55:22 +0600

    publishing awesome pages 1605599722195

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ApartmentDetails from './components/Apartment/ApartmentDetails';
+
+function App() {
+  return (
+    <div>
+      <h2>React Bootstrap </h2>
+        <ApartmentDetails/>
+    </div>
+  );
+}
+
+export default App;
